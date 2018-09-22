51098ea746295d3c3771255c1fc8d8e93cfff594
Lan <laguilar@uabc.edu.mx>
2018-09-22 12:04:50 -0700

    cambios

--- src/uabc/edu/mx/StepsForDay.java
+++ src/uabc/edu/mx/StepsForDay.java
@@ -4,26 +4,26 @@ public class StepsForDay {
     int steps;
     int goal;
 
-    StepsForDay(int goal) {
+    public StepsForDay(int goal) {
         this.goal = goal;
         this.steps = 0;
     }
 
-    void inputSteps(int steps) {
+    public void inputSteps(int steps) {
         this.steps = steps;
     }
 
-    void addSteps(int steps) {
-        this.steps = steps;
+    public void addSteps(int steps) {
+        this.steps += steps;
     }
 
-    void clearSteps(){
+    public void clearSteps(){
         this.steps = 0;
     }
 
-    int get() { return steps; }
+    public int get() { return steps; }
 
-    boolean metGoal(){
+    public boolean metGoal(){
         return steps >= goal;
     }
 
